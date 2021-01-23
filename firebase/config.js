6b5f842953cda4ca2112826a8986fb10de1d599d
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-23 13:31:33 -0500

    Configure firebase (apiKey...)

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDcirPfDISiUQ7yFo0oJ44kIt_ubQgufOk",
+    authDomain: "chat-react-20e52.firebaseapp.com",
+    projectId: "chat-react-20e52",
+    storageBucket: "chat-react-20e52.appspot.com",
+    messagingSenderId: "543276283070",
+    appId: "1:543276283070:web:bd5d3498ff3606db26daef"
+};
+
+export default firebaseConfig
