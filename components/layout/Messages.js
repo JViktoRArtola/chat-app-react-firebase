532941035a574d19dd4428ed4449283371cd2552
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-25 04:33:03 -0500

    Add Messages component

---
+++ components/layout/Messages.js
@@ -0,0 +1,27 @@
+import React, {useEffect} from 'react';
+import Message from "./Message";
+import {Input} from "./Input";
+import useMessages from "../../hooks/useMessages";
+
+const Messages = ({filter, user}) => {
+    const {messages} = useMessages('date', filter)
+    useEffect(() => {
+        const objDiv = document.getElementById("scrollable");
+        objDiv.scrollTop = objDiv.scrollHeight;
+    }, [messages]);
+
+    return (
+        messages ?
+            <div id='scrollable' style={{height: '100vh', overflowY: 'scroll', padding: '2% 0'}}>
+                <div style={{marginBottom: '7em'}}>
+                    {messages.filter(item => item.id !== 'Status').map((message, i) =>
+                        <Message key={i} message={message.text} owner={message.uid} user={user.uid} date={message.date}/>
+                    )}
+                </div>
+                <Input filter={filter} uid={user.uid}/>
+            </div> :
+           null
+    );
+};
+
+export default Messages;
