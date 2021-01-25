c4e68dde274009ff0630c542c670a113de8defd7
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-25 04:37:56 -0500

    Change index component

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,25 @@
-import React, {useState} from "react"
-import Router from 'next/router';
+import React, {useContext, useEffect, useState} from "react"
 import Layout from "../components/layout/Layout";
-import Button from "../components/ui/Button";
-import Loader from "../components/layout/Spinner";
-import {Form, Input, Error} from "../components/ui/Form";
-import firebase from "../firebase";
-import useValidation from "../hooks/useValidation";
-import validateLogin from "../validations/validateLogin";
-
-const INITIAL_STATE = {
-    email: '',
-    password: ''
-}
+import {Chat} from "../components/layout/Chat";
+import Login from "../components/layout/Login";
+import FirebaseContext from "../firebase/context";
+import Spinner from "../components/layout/Spinner";
 
 export default function Home() {
-    const [error, setError] = useState(false);
-    const {values, errors, handleSubmit, handleChange, handleBlur} = useValidation(INITIAL_STATE, validateLogin, startLogin);
-    const {email, password} = values;
+    const {user} = useContext(FirebaseContext);
+    const [loading, setLoading] = useState(true);
 
-    async function startLogin() {
-        try {
-            await firebase.login(email, password);
-            await Router.push('/');
-        } catch (error) {
-            console.error('Login Error ', error.message);
-            setError(error.message);
-        }
-    }
+    useEffect(() => {
+        setTimeout(function () {
+            setLoading(false)
+        }.bind(this), 2000);
+    }, []);
 
+    if (loading) return <Spinner/>
     return (
         <>
             <Layout>
-                <div className="joinOuterContainer">
-                    {error && <Error>{error} </Error>}
-                    <Loader/>
-                    <Form onSubmit={handleSubmit} noValidate>
-                        <h1 className="heading">Join</h1>
-                        <div>
-                            <Input id="email" name="email" placeholder="Email" type="text" value={email}
-                                   onChange={handleChange} onBlur={handleBlur}/>
-                            {errors.email && <Error>{errors.email}</Error>}
-                        </div>
-                        <div>
-                            <Input id="password" name="password" placeholder="Password" type="password" value={password}
-                                   onChange={handleChange} onBlur={handleBlur}/>
-                            {errors.password && <Error>{errors.password}</Error>}
-                        </div>
-                        <Button type="submit">Sign In</Button>
-                    </Form>
-                </div>
+                {!user ? <Login/> : <Chat/>}
             </Layout>
         </>
     )
