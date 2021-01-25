ae64e87489fe91d4165a3f59186b9a9da81304a4
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-25 04:35:46 -0500

    Add Input component

---
+++ components/layout/Input.js
@@ -0,0 +1,35 @@
+import React, {useContext, useState} from 'react';
+import styles from '../../styles/Input.module.css'
+import FirebaseContext from "../../firebase/context";
+
+
+export const Input = ({filter, uid}) => {
+    const [text, setText] = useState('');
+    const {firebase} = useContext(FirebaseContext);
+
+    function f() {
+        const msg = {
+            text,
+            date: new Date(),
+            uid: uid
+        }
+        firebase.db.collection(filter).add(msg);
+        setText('')
+    }
+
+    return (
+        <div style={{position: 'absolute', bottom: 20, width: '70%',}}>
+            <input
+                className={styles.input}
+                type="text"
+                placeholder="Type a message..."
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                onKeyPress={e => e.key === 'Enter' ? f() : null}
+            />
+            <button type='submit' className={styles.sendButton} onClick={() => f()}>
+                Send
+            </button>
+        </div>
+    )
+}
