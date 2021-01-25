109536478b556e5ae2a4ddfa500dd92b5c9b36f2
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-25 04:35:11 -0500

    Add useAuth to main page _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import firebase, {FirebaseContext} from "../firebase";
+import useAuth from "../hooks/useAuth";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    const user = useAuth();
+
+    return (
+        <FirebaseContext.Provider value={{firebase, user}}>
+            <Component {...pageProps} />
+        </FirebaseContext.Provider>
+    )
 }
 
 export default MyApp
