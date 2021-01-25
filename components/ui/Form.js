2bd702639ef223132e6c5877b48e4c70781ec0ba
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-25 04:30:01 -0500

    styles: change Styles

--- components/ui/Form.js
+++ components/ui/Form.js
@@ -21,3 +21,22 @@ export const Input = styled.input`
   padding: 15px 20px;
   width: 100%;
 `;
+
+
+export const Field = styled.div`
+    margin-bottom: 2rem;
+    display: flex;
+    align-items: center;
+    label {
+        flex: 0 0 150px;
+        font-size: 1.8rem;
+    }
+    input, 
+    textarea {
+        flex: 1;
+        padding: 1rem;
+    }
+    textarea {
+        height: 400px;
+    }
+`;
