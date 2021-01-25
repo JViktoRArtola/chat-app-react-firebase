6f867de0e0071c0e5f56983f765279244f35a19f
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-25 04:30:25 -0500

    styles: Improve Spinner

--- components/layout/Spinner.js
+++ components/layout/Spinner.js
@@ -1,4 +1,5 @@
-import styled, { keyframes } from 'styled-components'
+import React from 'react';
+import styled, {keyframes} from 'styled-components'
 
 const spin = keyframes`
     0% {
@@ -8,14 +9,29 @@ const spin = keyframes`
         transform: rotate(360deg);
     }
 `
-
-export const Loader = styled.div`
-  border: 0.5em solid rgba(0, 0, 0, 0.1);
-  border-top: 0.4em solid #767676;
+const Loader = styled.div`
+  border: 0.7em solid rgba(0, 0, 0, 0.1);
+  border-top: 0.6em solid #767676;
   border-radius: 50%;
-  width: 6.28571429rem;
-  height: 6.28571429rem;
+  width: 10.28571429rem;
+  height: 10.28571429rem;
   animation: ${spin} 0.6s linear infinite;
 `
+const Container = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+    align-items: center;
+    background-color: #1a1a1d;
+}
+`
+const Spinner = () => {
+    return (
+        <Container>
+            <Loader/>
+        </Container>
+    );
+};
 
-export default Loader
+export default Spinner;
