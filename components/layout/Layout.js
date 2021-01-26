7d4429c09f40e566184b5567ee57cfee85707ad6
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-25 23:44:13 -0500

    Add html tag lang

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -5,12 +5,13 @@ const Layout = props => {
     return (
         <>
             <Head>
+                <html lang="en"/>
                 <title>Chat React</title>
                 <link rel="icon" href="/favicon.ico"/>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
                       integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
                       crossOrigin="anonymous"/>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
                 <meta name='description' content="Web Chat created with Next Js and Google Firebase"/>
             </Head>
             <main>
