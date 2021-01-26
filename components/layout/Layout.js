025e1add8356585d015df2f51ffa785f6bba26c5
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-25 23:19:55 -0500

    Add meta tag description

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -10,6 +10,8 @@ const Layout = props => {
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
                       integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
                       crossOrigin="anonymous"/>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name='description' content="Web Chat created with Next Js and Google Firebase"/>
             </Head>
             <main>
                 {props.children}
