c66944e5c19663ba59466229e7fe217e0cd2c3d9
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-25 23:50:02 -0500

    Remove html tag

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -5,7 +5,6 @@ const Layout = props => {
     return (
         <>
             <Head>
-                <html lang="en"/>
                 <title>Chat React</title>
                 <link rel="icon" href="/favicon.ico"/>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
